2878e96dfe4b77d118693cf60432851940a5f1c9
Ariel Krispil <Arielk1177@gmail.com>
2023-03-30 01:55:18 +0300

    Add colors dataset

---
+++ src/datasets/colors.ts
@@ -0,0 +1,11 @@
+import { IdValuePair } from "../dto's/idValuePair";
+
+export type Color = "red" | "green" | "yellow" | "purple" | "cyan";
+
+export const colors: IdValuePair<Color>[] = [
+  { id: 0, value: "red" },
+  { id: 1, value: "green" },
+  { id: 2, value: "yellow" },
+  { id: 3, value: "purple" },
+  { id: 4, value: "cyan" },
+];
