c06e1e4ad6a4e31f23034a3653191271ed5e2b04
Ariel Krispil <Arielk1177@gmail.com>
2023-03-30 01:56:45 +0300

    remove unnecessary files and html content

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css';
+import { useSelector } from "react-redux";
+import { Todo } from "./components/todo";
+import { TodoState } from "./dto's/todoState";
 
 function App() {
+  const todo = useSelector((state: TodoState) => state.todos[0]);
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Todo {...todo}></Todo>
     </div>
   );
 }
