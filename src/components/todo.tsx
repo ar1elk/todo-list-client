585c03b5a43d445f2dc613a8797c7be6113b4e0f
ar1elk <Arielk1177@gmail.com>
2023-03-30 16:46:16 +0300

    Add remove todo button and styling

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -1,12 +1,20 @@
 import styled from "@emotion/styled";
 import CircleOutlinedIcon from "@mui/icons-material/CircleOutlined";
+import ClearIcon from "@mui/icons-material/Clear";
 import TaskAltOutlinedIcon from "@mui/icons-material/TaskAltOutlined";
 import { IconButton, MenuItem, Paper, Select, Typography } from "@mui/material";
-import { colors } from "../datasets/colors";
+import { useDispatch } from "react-redux";
+import { Color, colors } from "../datasets/colors";
 import { TodoItem } from "../dto's/todoItem";
+import { todoColored, todoRemoved } from "../redux/todosSlice";
 
 const CompleteTodoButton = styled(IconButton)`
   color: lime;
+  margin-right: 12px;
+`;
+
+const RemoveTodoButton = styled(IconButton)`
+  color: red;
 `;
 
 const TodoText = styled(Typography)`
@@ -15,20 +23,26 @@ const TodoText = styled(Typography)`
 
 const TodoPaper = styled(Paper)`
   display: flex;
-  justify-content: space-between;
   align-items: center;
-  width: 18%;
-  padding-right: 9px;
+  width: 19%;
   border: 1px solid #ccc;
 `;
 
 const ColorSelection = styled(Select)`
-  color: cyan;
-  width: 30%;
+  color: ${(props) => props.value as Color};
+  &:focus {
+    outline: 2px solid ${(props) => props.value as Color};
+  }
+
+  &.MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline {
+    border-color: ${(props) => props.value as Color};
+  }
   height: 30px;
+  margin-left: auto;
 `;
 
-export function Todo({ id, text, isCompleted, color }: TodoItem) {
+export function Todo({ id, text, isCompleted, color = "" }: TodoItem) {
+  const dispatch = useDispatch();
   return (
     <TodoPaper elevation={0}>
       <CompleteTodoButton>
@@ -44,6 +58,9 @@ export function Todo({ id, text, isCompleted, color }: TodoItem) {
         id="color-select"
         value={color}
         label="Color"
+        onChange={(e) =>
+          dispatch(todoColored({ id: id, color: e.target.value as Color }))
+        }
       >
         {colors.map((color) => (
           <MenuItem key={color.id} value={color.value}>
@@ -51,6 +68,9 @@ export function Todo({ id, text, isCompleted, color }: TodoItem) {
           </MenuItem>
         ))}
       </ColorSelection>
+      <RemoveTodoButton onClick={() => dispatch(todoRemoved(id))}>
+        <ClearIcon fontSize="large" />
+      </RemoveTodoButton>
     </TodoPaper>
   );
 }
