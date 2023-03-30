3f331ab88f125b1c6354d99b7fbcdc77bc2dfb13
ar1elk <Arielk1177@gmail.com>
2023-03-31 01:48:13 +0300

    Change green to lime

--- src/datasets/colors.ts
+++ src/datasets/colors.ts
@@ -1,10 +1,10 @@
 import { IdValuePair } from "../dto's/idValuePair";
 
-export type Color = "red" | "green" | "yellow" | "purple" | "cyan" | "";
+export type Color = "red" | "lime" | "yellow" | "purple" | "cyan" | "";
 
 export const colors: IdValuePair<Color>[] = [
   { id: 0, value: "red" },
-  { id: 1, value: "green" },
+  { id: 1, value: "lime" },
   { id: 2, value: "yellow" },
   { id: 3, value: "purple" },
   { id: 4, value: "cyan" },
