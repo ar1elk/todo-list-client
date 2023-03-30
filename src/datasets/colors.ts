05002ced94f3146376bb945505b9298c7ecc6d80
ar1elk <Arielk1177@gmail.com>
2023-03-30 16:45:23 +0300

    Add empty color

--- src/datasets/colors.ts
+++ src/datasets/colors.ts
@@ -1,6 +1,6 @@
 import { IdValuePair } from "../dto's/idValuePair";
 
-export type Color = "red" | "green" | "yellow" | "purple" | "cyan";
+export type Color = "red" | "green" | "yellow" | "purple" | "cyan" | "";
 
 export const colors: IdValuePair<Color>[] = [
   { id: 0, value: "red" },
