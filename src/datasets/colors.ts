525e2c124dd7f41e450df7e1c7a7e70b9f4a8437
ar1elk <Arielk1177@gmail.com>
2023-03-31 15:53:31 +0300

    Add more colors

--- src/datasets/colors.ts
+++ src/datasets/colors.ts
@@ -1,6 +1,17 @@
 import { IdValuePair } from "../dto's/idValuePair";
 
-export type Color = "red" | "lime" | "yellow" | "purple" | "cyan" | "";
+export type Color =
+  | "red"
+  | "lime"
+  | "yellow"
+  | "purple"
+  | "cyan"
+  | "black"
+  | "white"
+  | "blue"
+  | "maroon"
+  | "pink"
+  | "";
 
 export const colors: IdValuePair<Color>[] = [
   { id: 0, value: "red" },
@@ -8,4 +19,9 @@ export const colors: IdValuePair<Color>[] = [
   { id: 2, value: "yellow" },
   { id: 3, value: "purple" },
   { id: 4, value: "cyan" },
+  { id: 5, value: "black" },
+  { id: 6, value: "white" },
+  { id: 7, value: "blue" },
+  { id: 8, value: "maroon" },
+  { id: 9, value: "pink" },
 ];
